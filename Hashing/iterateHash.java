abe785adbc77c480fbc36045e08c9facd0f75a1d
NandeeshNB <nandeeshnb2005@gmail.com>
2026-05-27 12:27:21 +0530

    Iterate hash

---
+++ Hashing/iterateHash.java
@@ -0,0 +1,24 @@
+package Hashing;
+import java.util.*;
+public class iterateHash {
+    public static void main(String[] args) {
+        HashSet<String> hs=new HashSet<>();
+        hs.add("Nandeesh");
+        hs.add("nb");
+        hs.add("NB");
+        hs.add("Hatru");
+
+        //using iterator() method
+        Iterator<String> i=hs.iterator();
+        //traversing
+        while(i.hasNext()){
+            System.out.println(i.next()+", ");
+        }
+        System.out.println();
+
+        //using enhanced for loop or for each loop
+        for(String e:hs){
+            System.out.println(e+", ");
+        }
+    }
+}
