5920fb3f953b5fa44677f63be369a8589ac56126
NandeeshNB <nandeeshnb2005@gmail.com>
2026-05-27 12:07:23 +0530

    String hash

---
+++ Hashing/stringhash.java
@@ -0,0 +1,11 @@
+package Hashing;
+import java.util.*;
+
+public class stringhash {
+    public static void main(String args[]){
+        HashSet<String> hs=new HashSet<>();
+        hs.add("Nandeesh");
+        hs.add("NB");
+        System.out.println("HashSet : "+hs);
+    }
+}
