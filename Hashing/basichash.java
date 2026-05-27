364d734d758f0adde6063d704719cb6b1ebd336d
NandeeshNB <nandeeshnb2005@gmail.com>
2026-05-27 11:49:55 +0530

    basic hashset

---
+++ Hashing/basichash.java
@@ -0,0 +1,14 @@
+package Hashing;
+import java.util.*;
+
+public class basichash {
+    public static void main(String[] args) {
+        HashSet<Integer> hs=new HashSet<>();
+        hs.add(1);
+        hs.add(2);
+        hs.add(3);
+        hs.add(4);
+        System.out.println("Hash size : "+hs.size());
+        System.out.println("Elements in Hash set : "+hs);
+    }
+}
