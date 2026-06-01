036913625889df251fc4999d8ec27d27fdcd0b09
NandeeshNB <nandeeshnb2005@gmail.com>
2026-06-01 19:03:38 +0530

    Hash Map

---
+++ Hashing/hashmap_1.java
@@ -0,0 +1,18 @@
+package Hashing;
+import java.util.*;
+public class hashmap_1 {
+    public static void main(String args[]){
+
+        HashMap<String, Integer> hm=new HashMap<>();
+        
+        hm.put("jan",25 );
+        hm.put("jim",30);
+        hm.put("ran",45);
+
+        //Iteration
+        for(Map.Entry<String, Integer> entry:hm.entrySet()){
+            System.out.println(entry.getKey()+"->"+entry.getValue());
+        }
+
+    }
+}
