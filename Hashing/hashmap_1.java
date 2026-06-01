7d9e82f8e4d84274bfd0acb6344d56be134b9a2a
NandeeshNB <nandeeshnb2005@gmail.com>
2026-06-01 19:12:24 +0530

    Hash Map

--- Hashing/hashmap_1.java
+++ Hashing/hashmap_1.java
@@ -4,7 +4,7 @@ public class hashmap_1 {
     public static void main(String args[]){
 
         HashMap<String, Integer> hm=new HashMap<>();
-        
+
         hm.put("jan",25 );
         hm.put("jim",30);
         hm.put("ran",45);
@@ -13,6 +13,8 @@ public class hashmap_1 {
         for(Map.Entry<String, Integer> entry:hm.entrySet()){
             System.out.println(entry.getKey()+"->"+entry.getValue());
         }
+
+        System.out.println("Mappings : "+hm);
 
     }
 }
