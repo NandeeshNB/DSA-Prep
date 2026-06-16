4e157ddaccbf0de8cc562643b39698f4c41993df
NandeeshNB <nandeeshnb2005@gmail.com>
2026-06-16 11:34:19 +0530

    Linked list

---
+++ Linked List/insert_front_LL.java
@@ -0,0 +1,37 @@
+class Node{
+    int data;
+    Node next;
+    Node(int x){
+        this.data=x;
+        this.next=null;
+    }
+}
+public class insert_front_LL {
+    static Node insertAtFront(Node head, int x){
+        Node newNode=new Node(x);
+        newNode.next=head;
+        return newNode;
+    }
+    static void printlist(Node head){
+        Node temp=head;
+        while (temp!=null) {
+            System.out.print(temp.data);
+            if(temp.next!=null){
+                System.out.print("->");
+            }
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Node head= new Node(1);
+        head.next=new Node(2);
+        head.next.next=new Node(3);
+        head.next.next.next=new Node(4);
+        head.next.next.next.next=new Node(5);
+        int x=10;
+        printlist(head);
+        head=insertAtFront(head, x);
+        printlist(head);
+    }
+}
