14533f3ff273bbc6a1cb98b391c832808846be14
NandeeshNB <nandeeshnb2005@gmail.com>
2026-06-28 11:34:48 +0530

    Doubly Linked List

--- Linked List/DoubleLinked/forword_rec_trav.java
+++ Linked List/DoubleLinked/forword_rec_trav.java
@@ -1,5 +1,30 @@
 package DoubleLinked;
-
+class Node{
+    int data;
+    Node next;
+    Node prev;
+    Node(int x){
+        this.data=x;
+        this.next=this.prev=null;
+    }
+}
 public class forword_rec_trav {
-    
+    static void forwardTraversal(Node head){
+        if (head==null) {
+            return;
+        }
+        System.out.print(head.data+" ");
+        forwardTraversal(head.next);
+    }
+    public static void main(String[] args) {
+        Node head=new Node(1);
+        Node second=new Node(2);
+        Node third=new Node(3);
+        head.next=second;
+        second.prev=head;
+        second.next=third;
+        third.prev=second;
+        System.out.println("Forward Traversal");
+        forwardTraversal(head);
+    }
 }
