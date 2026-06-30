219966918dd21e06c63bdb24ee36301d4ad2556a
NandeeshNB <nandeeshnb2005@gmail.com>
2026-06-30 11:08:38 +0530

    Circular linked list

---
+++ Linked List/CircularLinked/delete_at_frontCL.java
@@ -0,0 +1,45 @@
+package CircularLinked;
+class Node{
+    int data;
+    Node next;
+    Node(int x){
+        data=x;
+        next=null;
+    }
+}
+public class delete_at_frontCL {
+    static Node deleteFront(Node last){
+        if (last==null) {
+            System.out.println("Emprty List");
+            return null;
+        }
+        Node head=last.next;
+        if (head==last) {
+            last=null;
+        }else{
+            last.next=head.next;
+        }
+        return last;
+    }
+    static void printList(Node last){
+        if(last==null) return;
+        Node head=last.next;
+        Node temp=head;
+        do{
+            System.out.print(temp.data);
+            temp=temp.next;
+            if(temp!=head) System.out.print("->");
+        }while(temp!=head);
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Node first=new Node(2);
+        first.next=new Node(3);
+        first.next.next=new Node(4);
+        Node last=first.next.next;
+        last.next=first;
+        printList(last);
+        last=deleteFront(last);
+        printList(last);
+    }
+}
