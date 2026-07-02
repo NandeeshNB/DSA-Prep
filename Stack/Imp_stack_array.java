ce9ddfb1a090660e9623f3a45fe23372a3cfac91
NandeeshNB <nandeeshnb2005@gmail.com>
2026-07-02 11:32:19 +0530

    Stack

--- Stack/Imp_stack_array.java
+++ Stack/Imp_stack_array.java
@@ -49,7 +49,8 @@ public class Imp_stack_array {
         st.push(2);
         st.push(3);
         st.push(4);
-        System.out.println("Popped : "+st.pop());
+        
+        //System.out.println("Popped : "+st.pop());
         System.out.println("Top element : "+st.peek());
         System.out.println("Is stack empty : "+(st.isEmpty()?"Yes":"No"));
         System.out.println("Is stack full : "+(st.isFull()?"Yes":"No"));
