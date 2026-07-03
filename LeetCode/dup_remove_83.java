1b54bf10b9243ef4c921e222ff086d44a7e8ded6
NandeeshNB <nandeeshnb2005@gmail.com>
2026-07-03 23:19:42 +0530

    leetcode

---
+++ LeetCode/dup_remove_83.java
@@ -0,0 +1,26 @@
+package LeetCode;
+public class ListNode {
+      int val;
+      ListNode next;
+     ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+public class dup_remove_83 {
+    
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                
+                current.next = current.next.next;
+            } else {
+                
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+}
